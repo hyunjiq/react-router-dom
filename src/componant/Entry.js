80a219c4c25d6cd295ad37e44359bce710653a99
hyunjiq <edalnim12@gmail.com>
2024-07-16 12:02:01 +0900

    버튼 배경 map

--- src/componant/Entry.js
+++ src/componant/Entry.js
@@ -1,21 +1,19 @@
 import React, {useState, useEffect} from 'react'
 
 function Entry() {
-    let count = 0; //일반 변수는 여전히 상태변수를 받지못해서
-    // useRef
-    const [num, setNum] = useState(0); //랜더링
-    useEffect(()=>{
-        document.body.style.backgroundColor = 'red'        
-    },[]);
-    useEffect(()=>{
-        document.querySelector('button').addEventListener("click",()=>{
-            setNum(num + 1);
-        })
-    },[num]); //랜더링 끝나고 나서 html태그로 이해
+    const bgcolor = ["red", "green", "blue"];
+    // useEffect(()=>{
+    //     document.querySelectorAll('li').forEach()
+    // }) element -> forEach
     return (
         <div>
-            <button >일반 변수 카운트 하나씩 증가 </button>
-            <h2>나는 일반 변수 count의 상태변수 값을 대입해서 출력 : {`${count = num} `}</h2>
+            {
+                bgcolor.map((v, i)=>{
+                    return(
+                        <button onClick={()=>{document.body.style.backgroundColor=v;}}>{v}</button>
+                    )
+                })
+            }
         </div>
     )
 }
