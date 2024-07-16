35d240429b387b17002d2dbbae6e35f5a7b04c94
hyunjiq <edalnim12@gmail.com>
2024-07-16 10:55:13 +0900

    a태그와 link태그 차이점

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import { Link, Routes, Route } from "react-router-dom";
 
 import About from './componant/About';
 import Board from './componant/Board';
+import Entry from "./componant/Entry";
 
 function App() {
   return (
     <div className="App">
       <div>
+        <a href="/">로고</a>
         <Link to="/about">회사소개</Link>
-        <Link to="/board">공지사항</Link>
+        <Link to="/board">공지사항</Link>        
       </div>
       <Routes>
-        <Route path='/about' element={ <About></About> }>공지사항</Route>
-        <Route path='/board' element={ <Board></Board> }>회사소개</Route>
+        <Route path='/' element={ <Entry></Entry> }></Route>
+        <Route path='/about' element={ <About></About> }></Route>
+        <Route path='/board' element={ <Board></Board> }></Route>
       </Routes>
-      
+      {/*Routes 바깥에 쓴 컴포넌트는 항상 노출*/}
+      <aside>나는 퀵</aside>
+      <footer>나는 하단</footer>
     </div>
   );
 }
---
+++ src/componant/Entry.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Entry() {
+    
+    return (
+        <h2>나는 첫페이지 컨텐츠 입니다.</h2>
+    )
+}
+
+export default Entry
