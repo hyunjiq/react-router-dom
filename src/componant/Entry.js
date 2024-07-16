87dd4a32e15f270dddf06bb299033c8239c6d523
hyunjiq <edalnim12@gmail.com>
2024-07-16 11:36:37 +0900

    화살표함수내에서 저장안됨 count= num

--- src/componant/Entry.js
+++ src/componant/Entry.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Entry() {
-    
+    let count = 0; //일반 변수는 여전히 상태변수를 받지못해서
+    // useRef
+    const [num, setNum] = useState(0); //랜더링
+    useEffect(()=>{
+        document.querySelector('button').addEventListener("click",()=>{
+            
+        })
+    },[]); //랜더링 끝나고 나서 html태그로 이해
     return (
-        <h2>나는 첫페이지 컨텐츠 입니다.</h2>
+        <div>
+            <button onClick={
+                ()=>{ 
+                    setNum(num + 1); // num = 10; 상태변수의 값을 변경하는 상태함수
+                    count = num; // 화살표 함수 내에서 저장 안됨
+                    }
+                }>일반 변수 카운트 하나씩 증가 </button>
+            <h2>나는 일반 변수 count의 상태변수 값을 대입해서 출력 : {`${count = num} `}</h2>
+        </div>
     )
 }
 
