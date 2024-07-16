ae00adad25f206a6a733b84a9c9bce792ffc16de
hyunjiq <edalnim12@gmail.com>
2024-07-16 11:41:29 +0900

    useEffect로 버튼 클릭이벤트

--- src/componant/Entry.js
+++ src/componant/Entry.js
@@ -5,18 +5,16 @@ function Entry() {
     // useRef
     const [num, setNum] = useState(0); //랜더링
     useEffect(()=>{
+        document.body.style.backgroundColor = 'red'        
+    },[]);
+    useEffect(()=>{
         document.querySelector('button').addEventListener("click",()=>{
-            
+            setNum(num + 1);
         })
-    },[]); //랜더링 끝나고 나서 html태그로 이해
+    },[num]); //랜더링 끝나고 나서 html태그로 이해
     return (
         <div>
-            <button onClick={
-                ()=>{ 
-                    setNum(num + 1); // num = 10; 상태변수의 값을 변경하는 상태함수
-                    count = num; // 화살표 함수 내에서 저장 안됨
-                    }
-                }>일반 변수 카운트 하나씩 증가 </button>
+            <button >일반 변수 카운트 하나씩 증가 </button>
             <h2>나는 일반 변수 count의 상태변수 값을 대입해서 출력 : {`${count = num} `}</h2>
         </div>
     )
